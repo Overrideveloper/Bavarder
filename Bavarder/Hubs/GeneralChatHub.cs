3496614e5cdd214957f9d750497d0cff405edf55
overrideveloper <bansowisdom@gmail.com>
2017-09-30 22:45:14 +0100

    Added active users tracking functionality

--- Bavarder/Hubs/GeneralChatHub.cs
+++ Bavarder/Hubs/GeneralChatHub.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using Bavarder.Models;
+using System.Threading.Tasks;
 
 namespace Bavarder.Hubs
 {
     public class GeneralChatHub : Hub
     {
+        private static int _userCount = 0;
         public void Send(string uid, string message)
         {
             var appDb = new ApplicationDbContext();
@@ -16,6 +18,33 @@ namespace Bavarder.Hubs
             var username = user.UserName;
             var photo = user.UserPhoto;
             Clients.All.newMessage(uid, username, photo, message);
+        }
+
+        public override Task OnConnected()
+        {
+            _userCount++;
+            var name = Context.User.Identity.Name;
+            var context = GlobalHost.ConnectionManager.GetHubContext<GeneralChatHub>();
+            context.Clients.All.online(_userCount, name);
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            _userCount--;
+            var name = Context.User.Identity.Name;
+            var context = GlobalHost.ConnectionManager.GetHubContext<GeneralChatHub>();
+            context.Clients.All.offline(_userCount, name);
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {
+            _userCount++;
+            var name = Context.User.Identity.Name;
+            var context = GlobalHost.ConnectionManager.GetHubContext<GeneralChatHub>();
+            context.Clients.All.online(_userCount, name);
+            return base.OnReconnected();
         }
     }
 }
