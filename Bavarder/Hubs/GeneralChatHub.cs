2076a3ec37a5ae6069ef69184eb07e8c4d8fbb86
overrideveloper <bansowisdom@gmail.com>
2017-10-02 23:01:07 +0100

    Added GeneralChat js models && viewmodels and server side chat methods

--- Bavarder/Hubs/GeneralChatHub.cs
+++ Bavarder/Hubs/GeneralChatHub.cs
@@ -5,46 +5,79 @@ using System.Web;
 using Microsoft.AspNet.SignalR;
 using Bavarder.Models;
 using System.Threading.Tasks;
+using Bavarder.Models.ChatModels;
+using Microsoft.AspNet.Identity;
+using Bavarder.ChatUtilities;
 
 namespace Bavarder.Hubs
 {
     public class GeneralChatHub : Hub
     {
-        private static int _userCount = 0;
-        public void Send(string uid, string message)
+        private static int _userCount;
+        private InMemoryRepository _context;
+
+        #region constructor
+        public GeneralChatHub()
         {
+            _context = InMemoryRepository.GetContext();
+            _userCount = 0;
+        }
+        #endregion
+
+        public void Send(ChatMessage message)
+        {
+            string uid = _context.GetUserByConnectionId(Context.ConnectionId);
             var appDb = new ApplicationDbContext();
             var user = appDb.Users.Find(uid);
             var username = user.UserName;
             var photo = user.UserPhoto;
-            Clients.All.newMessage(uid, username, photo, message);
-        }
-
-        public override Task OnConnected()
-        {
-            _userCount++;
-            var name = Context.User.Identity.Name;
-            var context = GlobalHost.ConnectionManager.GetHubContext<GeneralChatHub>();
-            context.Clients.All.online(_userCount, name);
-            return base.OnConnected();
+            if (!string.IsNullOrEmpty(message.Content))
+            {
+                message.Content = HttpUtility.HtmlEncode(message.Content);
+                HashSet<string> extractUrls;
+                message.Content = TextParser.TransformAndExtractUrls(message.Content, out extractUrls);
+                message.TimeStamp = DateTime.Now;
+                Clients.All.newMessage(uid, username, photo, message);
+            }
         }
 
         public override Task OnDisconnected(bool stopCalled)
         {
-            _userCount--;
-            var name = Context.User.Identity.Name;  
-            var context = GlobalHost.ConnectionManager.GetHubContext<GeneralChatHub>();
-            context.Clients.All.offline(_userCount, name);
+            string uid = _context.GetUserByConnectionId(Context.ConnectionId);
+            if (uid != null)
+            {
+                ChatUser user = _context.Users.Where(u => u.Id == uid).FirstOrDefault();
+                if (user != null)
+                {
+                    _context.RemoveUser(user);
+                    _userCount--;
+                    return Clients.All.leaves(user.Id, user.Username, DateTime.Now);
+                }
+            }
             return base.OnDisconnected(stopCalled);
         }
 
-        public override Task OnReconnected()
+        public int UserCount()
         {
-            _userCount++;
-            var name = Context.User.Identity.Name;
-            var context = GlobalHost.ConnectionManager.GetHubContext<GeneralChatHub>();
-            context.Clients.All.online(_userCount, name);
-            return base.OnReconnected();
+            return _userCount;
+        }
+
+        public void Joined()
+        {
+            ChatUser user = new ChatUser()
+            {
+                Id = Context.User.Identity.GetUserId(),
+                Username = Context.User.Identity.GetUserName()
+            };
+
+            _context.AddUser(user);
+            _context.AddMapping(Context.ConnectionId, user.Id);
+            Clients.All.Joins(user.Id, Context.User.Identity.GetUserName(), DateTime.Now);
+        }
+
+        public ICollection<ChatUser> GetConnectedUsers()
+        {
+            return _context.Users.ToList<ChatUser>();
         }
     }
 }
---
+++ Bavarder/Scripts/ChatScripts/general.js
@@ -0,0 +1,34 @@
+﻿var genChat = {}
+
+//Models
+getChat.chatMessage = function (sender, content, dateSent) {
+    var self = this;
+    self.username = sender;
+    self.content = content;
+    if (dateSent != null) {
+        self.timestamp = dateSent;
+    }
+}
+
+genChat.user = function (username, userId) {
+    var self = this;
+    self.username = username;
+    self.id = userId;
+}
+
+//Viewmodels
+genChat.chatViewModel = function () {
+    var self = this;
+    self.this = ko.observableArray();
+}
+
+genChat.connectedUsersViewodel = function () {
+    var self = this;
+    self.contacts = ko.observableArray();
+    self.customRemove = function (userToRemove) {
+        var userIdToRemove = userToRemove.Id;
+        self.contacts.remove(function (item) {
+            return item.id === userIdToRemove;
+        });
+    }
+}
