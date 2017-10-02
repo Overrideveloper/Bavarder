744bf9b8fc83b8e51a446e5bcd52352872efa679
overrideveloper <bansowisdom@gmail.com>
2017-10-02 21:29:28 +0100

    Added Chat models

---
+++ Bavarder/Models/ChatModels/ChatMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bavarder.Models.ChatModels
+{
+    public class ChatMessage
+    {
+        public string Username { get; set; }
+        public string Content { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
---
+++ Bavarder/Models/ChatModels/InMemoryRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bavarder.Models.ChatModels
+{
+    public class InMemoryRepository
+    {
+        public static ICollection<ChatUser> _connectedUsers;
+        private static Dictionary<string, string> _mappings;
+        private static InMemoryRepository _context = null;
+
+        #region create instance of class
+        public static InMemoryRepository GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new InMemoryRepository();
+            }
+            return _context;
+        }
+        #endregion
+
+        #region constructor
+        private InMemoryRepository()
+        {
+            _connectedUsers = new List<ChatUser>();
+            _mappings = new Dictionary<string, string>();
+        }
+        #endregion
+
+        #region repo methods
+        public IQueryable<ChatUser> Users
+        {
+            get 
+            {
+                return _connectedUsers.AsQueryable();
+            }
+        }
+
+        public void AddUser(ChatUser user)
+        {
+            _connectedUsers.Add(user);
+        }
+
+        public void RemoveUser(ChatUser user)
+        {
+            _connectedUsers.Remove(user);
+        }
+
+        public void AddMapping(string connectionId, string userId)
+        {
+            if (!string.IsNullOrEmpty(connectionId) && !string.IsNullOrEmpty(userId))
+            {
+                _mappings.Add(connectionId, userId);
+            }
+        }
+
+        public string GetUserByConnectionId(string connectionId)
+        {
+            string userId = null;
+            _mappings.TryGetValue(connectionId, out userId);
+            return userId;
+        }
+        #endregion
+    }
+}
